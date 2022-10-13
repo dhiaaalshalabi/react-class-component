e531f932d9bc118023332b58cd0a4dd4f6ad2053
Dhiaa Al-Shalabi <dhiaa.alshalabi@gmail.com>
2022-10-13 20:18:49 +0300

    Convert to a class component with state.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React from 'react'
 import './App.css'
 
 class App extends React.Component {
-
   state = {
-    goOut: true
+    count: 0
   }
 
-  toggleGoOut = () => {
-    this.setState(prevState => {
-      return {
-        goOut: !prevState.goOut
-      }
-    })
+  add = () => {
+    this.setState(preCount => ({ count: preCount.count + 1 }))
+  }
+
+  subtract = () => {
+    this.setState(preCount => ({ count: preCount.count - 1 }))
   }
 
   render() {
     return (
-      <div className="state">
-        <h1 className="state-title">Should I go out tonight?</h1>
-        <div className="state-value" onClick={this.toggleGoOut}>
-          <h1>{this.state.goOut ? "Yes" : "No"}</h1>
+      <div className="counter" >
+        <button className="counter--minus" onClick={this.subtract}>–</button>
+        <div className="counter--count">
+          <h1>{this.state.count}</h1>
         </div>
+        <button className="counter--plus" onClick={this.add}>+</button>
       </div>
     )
   }
